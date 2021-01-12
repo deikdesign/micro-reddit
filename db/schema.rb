d544e1556c7895994160af6847d636d449d2bd42
SunnySparks <josediazt6@outlook.com>
2021-01-12 14:23:27 -0600

    Add validation for posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_224639) do
+ActiveRecord::Schema.define(version: 2021_01_12_193450) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "author_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
